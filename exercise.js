9f0707499ec065829e70974eb6bd7276cfecd3eb
garatina2202 <garatina2202@users.noreply.github.com>
2021-03-18 19:35:16 +1300

    Update exercise.js
    passed the getQuestions method

--- exercise.js
+++ exercise.js
@@ -1,4 +1,11 @@
 function getCategories(numOfCategories){
-    categories = fetchCategoriesFromAPI(numOfCategories)
+    let categories = fetchCategoriesFromAPI(numOfCategories)
     return categories
+}
+
+function getQuestions(category){
+    for (let i = 0; i < length(category); i+=1){
+        question[i] = fetchQuestionsFromAPI(category)
+    }
+    return question
 }
